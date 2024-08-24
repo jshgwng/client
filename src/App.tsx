ff9a57ff625d103922de955ee56abe9e8b0f2921
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-08-25 00:32:44 +0300

    Add: setup dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import { createTheme, ThemeProvider } from "@mui/material/styles"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/system";
+import Navbar from "@/scenes/navbar/index";
+import Dashboard from "@/scenes/dashboard/index";
 
 function App() {
 const theme = useMemo(()=>createTheme(themeSettings),[]);
   return (
     <div className='app'>
+      <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline/>
+        <Box width={"100%"} height={"100%"} padding={"1rem 2rem 4rem 2rem"}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path="/predictions" element={<div>predictions page</div>}/>
+          </Routes>
+        </Box>
       </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
